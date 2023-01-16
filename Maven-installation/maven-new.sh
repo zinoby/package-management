8da7fa4e5f0b0a402f78466015137352d66916ae
zinoby <118639166+zinoby@users.noreply.github.com>
2023-01-16 07:56:48 -0500

    Update maven-new.sh

--- Maven-installation/maven-new.sh
+++ Maven-installation/maven-new.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+# install Java JDK 1.8+ as a pre-requisit for maven to run.
+
+sudo hostnamectl set-hostname maven
+sudo su - ec2-user
+cd /opt
+sudo yum install wget nano tree unzip git-all -y
+sudo yum install java-11-openjdk-devel java-1.8.0-openjdk-devel -y
+java -version
+git --version
