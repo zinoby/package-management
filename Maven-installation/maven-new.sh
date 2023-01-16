705152852b89f83c1cc8c7304e9a7a72eff4e13d
zinoby <118639166+zinoby@users.noreply.github.com>
2023-01-16 07:57:53 -0500

    Update maven-new.sh

--- Maven-installation/maven-new.sh
+++ Maven-installation/maven-new.sh
@@ -8,3 +8,8 @@ sudo yum install wget nano tree unzip git-all -y
 sudo yum install java-11-openjdk-devel java-1.8.0-openjdk-devel -y
 java -version
 git --version
+#Step1) Download the Maven Software
+sudo wget https://dlcdn.apache.org/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.zip
+sudo unzip apache-maven-3.8.7-bin.zip
+sudo rm -rf apache-maven-3.8.7-bin.zip
+sudo mv apache-maven-3.8.7/ maven
